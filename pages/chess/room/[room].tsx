c99fdc22b5bca690c27672bcf3d93f84b8b58715
Gabrieldrc <gabriel.drc25@gmail.com>
2022-02-04 22:28:59 -0300

    FNT-0009 room page fetch first data

--- pages/chess/room/[room].tsx
+++ pages/chess/room/[room].tsx
@@ -1,11 +1,29 @@
-import {NextPage} from 'next'
-import { useRouter } from 'next/router'
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { chessClient } from "../../../core/services/ChessClient";
+import SocketService from "../../../core/services/SocketService";
 const Room: NextPage = () => {
-  const router = useRouter()
-  const { room } = router.query
-  return (
-    <h1>{room}</h1>
-  )
-}
+  const router = useRouter();
+  const { room } = router.query;
+  const [turn, setTurn] = useState(null);
+  const [board, setBoard] = useState(null);
 
-export default Room
+  useEffect(() => {
+    async function fetchInitialState() {
+      const resp = await chessClient.getState(`${room}`);
+      if (resp.status == 200) {
+        setBoard(resp.data['response']['board'])
+        setTurn(resp.data['response']['turn'])
+      }
+    }
+    fetchInitialState();
+    // SocketService.listen()
+  }, []);
+
+  return (
+    <h1>{`Share your room code: ${room}`}</h1>
+  );
+};
+
+export default Room;
