a37040e8dd3541cfe7a6ada839f071af78211574
Gabrieldrc <gabriel.drc25@gmail.com>
2022-02-23 17:03:20 -0300

    FNT.0019 se añadio el componente room code que te permite seleccionar o copiar el codigo

--- components/RoomCodeComponent/RoomCodeComponent.tsx
+++ components/RoomCodeComponent/RoomCodeComponent.tsx
@@ -1,20 +1,46 @@
 import { NextComponentType } from "next";
 import style from "./RoomCodeComponent.module.sass";
 import Image from "next/image";
-import clipboard from "/public/icons/clipboard.svg";
+import clipboardSrc from "/public/icons/clipboard-fill.svg";
+import clipboardCheckSrc from "/public/icons/clipboard-check-fill.svg";
+import { useState } from "react";
 
 const RoomCodeComponent: NextComponentType = ({ roomCode }) => {
+  const [imgSrc, setImgSrc] = useState(clipboardSrc);
+  const [showPopout, setShowPopout] = useState(false);
   const copiarAlPortapapeles = () => {
     navigator.clipboard.writeText(roomCode);
+    setImgSrc(clipboardCheckSrc);
+    setShowPopout(true)
+    setTimeout(()=> {
+      setImgSrc(clipboardSrc)
+      setShowPopout(false)
+    }, 3000)
+  };
 
+  const selectContent = (e: Event) => {
+    e.preventDefault();
+    e.target.select();
+    e.target.setSelectionRange(0, 99999);
   };
   return (
     <div className={style.container}>
       <span>room code</span>
-      <span className={style.roomCode} id="roomCode">{roomCode}</span>
-      <div  className={style.iconContainer} onClick={copiarAlPortapapeles}>
-        <Image alt="clipboard-icon" src={clipboard} layout="fixed"/>
+      <input
+        type="text"
+        onClick={selectContent}
+        className={style.roomCode}
+        id="roomCode"
+        value={roomCode}
+        readOnly
+      />
+      <div className={style.button} onClick={copiarAlPortapapeles}>
+        <Image alt="clipboard-icon" src={imgSrc} layout="fixed" />
       </div>
+      {
+        !showPopout? "" :
+        <div className={style.popout}>copied!</div>
+      }
     </div>
   );
 };
